3313dea0dda6ba3d91c6cb3feea7f84db1563346
FilderZee <filipveru@gmail.com>
2024-09-04 22:00:34 +0200

    Přidejte soubory projektu.

---
+++ ToDoList/ToDoApp.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    internal class ToDoApp 
+    {
+        public void Start()
+        {
+            ToDoList todolist = new ToDoList(); 
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("To-Do List v1.0");
+                Console.WriteLine("---------------");
+                Console.WriteLine("Vyberte akci, kterou chcete provést:");
+                Console.WriteLine("(1) Přidat položku");
+                Console.WriteLine("(2) Odebrat položku");
+                Console.WriteLine("(3) Ukázat seznam");
+                Console.WriteLine("(4) Ukončit program");
+
+                int userChoice;
+                bool isValidInput = int.TryParse(Console.ReadLine(), out userChoice);
+
+                if (!isValidInput)
+                {
+                    Console.WriteLine("Neplatná volba, zkuste to znovu.");
+                    continue;
+                }
+
+                switch (userChoice)
+                {
+                    case 1:
+                        Console.Clear();
+                        todolist.AddItem();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        todolist.DeleteItem();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        todolist.ShowList();
+                        Console.WriteLine("Stiskněte libovolnou klávesu pro návrat do hlavního menu.");
+                        Console.ReadKey();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.WriteLine("Program ukončen.");
+                        return;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Neplatná volba, zkuste to znovu.");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ToDoList/ToDoList.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    internal class ToDoList
+    {
+        List<string> itemList = new List<string>();
+        private int maxItemCount = 10;
+   
+
+        public int MaxItemCount { get { return maxItemCount; } set { maxItemCount = value; } }
+
+        public void AddItem()
+        {
+            if (itemList.Count >= maxItemCount)
+            {
+                Console.WriteLine("Nelze přidat více položek. Seznam je plný.");
+                return;
+            }
+
+            Console.Write("Napište název položky, kterou chcete přidat: ");
+            string item = Console.ReadLine();
+            itemList.Add(item);
+        }
+
+
+        public void ShowList()
+        {
+            int itemIndex = 1; 
+            foreach (string item in itemList)
+            {
+                Console.WriteLine(itemIndex + ". " + item);
+                itemIndex++;
+            }
+        }
+
+
+        public void DeleteItem()
+        {
+            ShowList();
+            Console.Write("Napište index položky, kterou chcete odstranit: ");
+            int userDeleteChoice = Convert.ToInt32(Console.ReadLine()) - 1;
+
+            if (userDeleteChoice >= 0 && userDeleteChoice < itemList.Count)
+            {
+                itemList.RemoveAt(userDeleteChoice);
+                Console.WriteLine("Položka byla úspěšně odstraněna.");
+            }
+            else
+            {
+                Console.WriteLine("Neplatný index.");
+            }
+        }
+
+    }
+}
